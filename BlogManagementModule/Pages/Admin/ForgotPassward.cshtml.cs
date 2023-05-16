fd12e4e557975bd0b7e505e75dca8cddccebb34d
bjsoft1@gmail.com <bjsoft1@gmail.com>
2023-05-16 08:25:32 +0545

    Initial Razor Pages Setup

---
+++ BlogManagementModule/Pages/Admin/ForgotPassward.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogManagementModule.Pages.Admin
+{
+    public class ForgotPasswardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BlogManagementModule/Pages/Admin/SiteSettings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogManagementModule.Pages.Admin
+{
+    public class SiteSettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
